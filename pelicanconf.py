1f7cee3bdca7a1d68a274dde72187da0c7033f5a
Iacchus Mercurius <kassivs@gmail.com>
2017-05-05 15:26:46 -0300

    initial commit :tada:

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Iacchus Mercurius'
+SITENAME = "The Philosopher's Workbench"
+SITEURL = 'https://iacchus.github.io'
+
+PATH = 'content'
+OUTPUT_PATH = 'site' # also in Makefile
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 11
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
