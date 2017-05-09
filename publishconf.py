f76192c7e2bc3c65f626098e1bdb651eff88db97
Iacchus Mercurius <kassivs@gmail.com>
2017-05-09 02:31:45 -0300

    ok

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://iacchus.github.io/site'
-#RELATIVE_URLS = False
-RELATIVE_URLS = True
+RELATIVE_URLS = False
+#RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
