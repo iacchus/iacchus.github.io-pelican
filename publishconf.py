d14ae74f41ec61a13f073d34ba22459870632367
Iacchus Mercurius <kassivs@gmail.com>
2017-05-09 02:20:45 -0300

    going

--- publishconf.py
+++ publishconf.py
@@ -11,12 +11,14 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://iacchus.github.io/site'
-RELATIVE_URLS = False
+#RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+#DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
