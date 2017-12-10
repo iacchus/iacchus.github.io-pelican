9c434d768c0b70fdf73e337869412bb1b174fcbc
Iacchus Mercurius <kassivs@gmail.com>
2017-12-10 19:47:03 -0200

    okz

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,14 +29,14 @@ PLUGIN_PATHS = [
 ]
 
 PLUGINS = [
-    'just_table',
+    #'just_table',
     'liquid_tags.youtube',
-    'liquid_tags.notebook',
-    'ipynb.markup',
+    #'liquid_tags.notebook',
+    #'ipynb.markup',
     'pin_to_top',
     # 'post_revision',
-    'render_math',
-    'rmd_reader',
+    #'render_math',
+    #'rmd_reader',
     'show_source',
     # 'summary',
 ]
