f86b24166da2b90573bd04725a4384118e1aa51b
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-11 17:29:52 -0300

    ok

---
+++ build-for-travis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -e
+
+#source ~/git-stuff/jupyter-venv/venv/bin/activate
+#source ~/git-stuff/VENV/bin/activate
+
+make clean
+make publish
+
+#git add .
+#git commit -a -m 'Autocommit from build.sh'
+#git push
+
+cd ../iacchus.github.io
+git add .
+git commit -a -m 'Autocommit from build-for-travis.sh'
+git push
+
+cd -
+
