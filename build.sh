45cc0927ea492b52e4ca4e5274aa8d6db3cba84b
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-17 21:02:23 -0300

    Autocommit from build.sh

--- build.sh
+++ build.sh
@@ -3,20 +3,18 @@
 # exit on error
 set -e
 
-#source ~/git-stuff/jupyter-venv/venv/bin/activate
-#source ~/git-stuff/VENV/bin/activate
 
 make clean
 make publish
 
 git add .
 git commit -a -m 'Autocommit from build.sh' || true
-git push || true
+git push
 
 cd ../iacchus.github.io
 git add .
 git commit -a -m 'Autocommit from build.sh' || true
-git push || true
+git push
 
 cd -
 
---
+++ content/are-you-reaady.md
@@ -0,0 +1,8 @@
+title: are you reaady?
+date: Fri 17 Jul 2020 09:01:12 PM -03
+modified: Fri 17 Jul 2020 09:01:12 PM -03
+tags: last tests, before, launch
+summary: we are about tu launch our new developed website
+status: published
+
+<marquee>soooooon.......</marquee>
