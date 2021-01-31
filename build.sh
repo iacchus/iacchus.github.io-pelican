62d1b93c46878dd490d6a7ee2dd9544a9beaee47
I.M <=>
2021-01-31 20:58:00 -0300

    Autocommit from build.sh

--- build.sh
+++ build.sh
@@ -3,21 +3,24 @@
 # exit on error
 set -e
 
+export PUBDIR="../iacchus.github.io"
+
 # push actual state
 
 git add .
 git commit -a -m 'Autocommit from build.sh' || true
 git push
 
-# clean old published
 
 make clean
 
-# republish
-
-git init ../iacchus.github.io
+#rm -rf $PUBDIR
+git clone git@github.com:iacchus/iacchus.github.io.git $PUBDIR 
+make clean
 
 make publish
+#git init ../iacchus.github.io
+#git -C ../iacchus.github.io remote add origin 
 
 cd ../iacchus.github.io
 
