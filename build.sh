ac69614e3afb6aa5b7a140dbda4e9058f5f7761b
I.M <=>
2021-01-31 20:45:41 -0300

    Autocommit from build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,26 @@
 # exit on error
 set -e
 
-
-make clean
-make publish
+# push actual state
 
 git add .
 git commit -a -m 'Autocommit from build.sh' || true
 git push
 
+# clean old published
+
+make clean
+
+# republish
+
+git init ../iacchus.github.io
+
+make publish
+
 cd ../iacchus.github.io
+
 git add .
 git commit -a -m 'Autocommit from build.sh' || true
 git push
 
 cd -
-
